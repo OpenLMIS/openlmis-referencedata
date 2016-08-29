9d742a649e1cd424e01fd999a4e14842e480df21
Chongsun Ahn <cahn922@yahoo.com>
2016-08-29 14:53:25 -0700

    OLMIS-545 Add get all supervised facilities
    Add method to get all supervised facilities for this supervisory node. Add test case.
    
    Also add requisition group property to SupervisoryNode, and add static factory method for object creation. Add addChildNode method to add child supervisory nodes (with reference to parent node).

---
+++ src/test/java/org/openlmis/referencedata/domain/SupervisoryNodeTest.java
@@ -0,0 +1,35 @@
+package org.openlmis.referencedata.domain;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SupervisoryNodeTest {
+
+  @Test
+  public void shouldGetAllSupervisedFacilities() {
+    //given
+    SupervisoryNode supervisoryNode1 = SupervisoryNode.newSupervisoryNode(new Facility());
+    RequisitionGroup requisitionGroup1 = RequisitionGroup.newRequisitionGroup(supervisoryNode1, 
+        null, Arrays.asList(new Facility(), new Facility()));
+    supervisoryNode1.assignRequisitionGroup(requisitionGroup1);
+
+    SupervisoryNode supervisoryNode2 = SupervisoryNode.newSupervisoryNode(new Facility());
+    RequisitionGroup requisitionGroup2 = RequisitionGroup.newRequisitionGroup(supervisoryNode2,
+        null, Collections.singletonList(new Facility()));
+    supervisoryNode2.setRequisitionGroup(requisitionGroup2);
+
+    supervisoryNode1.addChildNode(supervisoryNode2);
+    
+    //when
+    List<Facility> facilities = supervisoryNode1.getAllSupervisedFacilities();
+
+    //then
+    assertThat(facilities.size(), is(3));
+  }
+}
