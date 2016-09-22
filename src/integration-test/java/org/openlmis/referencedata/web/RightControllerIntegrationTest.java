39796c554c53401775e4975581c4a237753ce758
Chongsun Ahn <cahn922@yahoo.com>
2016-09-22 16:15:08 -0700

    OLMIS-1017 Update right controller integration tests
    Update tests to test updated endpoints and to match RAML. Make slight changes to RAML to match desired behavior implemented by endpoints.

--- src/integration-test/java/org/openlmis/referencedata/web/RightControllerIntegrationTest.java
+++ src/integration-test/java/org/openlmis/referencedata/web/RightControllerIntegrationTest.java
@@ -1,165 +1,185 @@
 package org.openlmis.referencedata.web;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Assert;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openlmis.referencedata.domain.Right;
 import org.openlmis.referencedata.domain.RightType;
+import org.openlmis.referencedata.dto.RightDto;
 import org.openlmis.referencedata.repository.RightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
 import guru.nidi.ramltester.junit.RamlMatchers;
-import java.util.Arrays;
-import java.util.UUID;
 
-@Ignore
+import java.util.Arrays;
+import java.util.List;
+
 public class RightControllerIntegrationTest extends BaseWebIntegrationTest {
 
   private static final String RESOURCE_URL = "/api/rights";
   private static final String ID_URL = RESOURCE_URL + "/{id}";
   private static final String ACCESS_TOKEN = "access_token";
-  private static final UUID ID = UUID.fromString("1752b457-0a4b-4de0-bf94-5a6a8002427e");
+  private static final String RIGHT_NAME = "right";
   private static final String DESCRIPTION = "OpenLMIS";
 
   @Autowired
   private RightRepository rightRepository;
 
-  private Right right = Right.newRight("rightName", RightType.GENERAL_ADMIN);
+  private Right right;
+  private RightDto rightDto;
 
+  /**
+   * Constructor for test class.
+   */
+  public RightControllerIntegrationTest() {
+    right = Right.newRight(RIGHT_NAME, RightType.GENERAL_ADMIN);
+    rightDto = new RightDto();
+    right.export(rightDto);
+  }
+  
   @Before
   public void setUp() {
     rightRepository.save(right);
+    rightRepository.save(Right.newRight("right2", RightType.SUPERVISION));
   }
 
-  @Test
-  public void shouldDeleteRight() {
-
-    restAssured.given()
-          .queryParam(ACCESS_TOKEN, getToken())
-          .contentType(MediaType.APPLICATION_JSON_VALUE)
-          .pathParam("id", right.getId())
-          .when()
-          .delete(ID_URL)
-          .then()
-          .statusCode(204);
-
-    Assert.assertFalse(rightRepository.exists(right.getId()));
-    assertThat(RAML_ASSERT_MESSAGE, restAssured.getLastReport(), RamlMatchers.hasNoViolations());
-  }
-
-  @Test
-  public void shouldNotDeleteNonexistentRight() {
-
-    rightRepository.delete(right);
-
-    restAssured.given()
-          .queryParam(ACCESS_TOKEN, getToken())
-          .contentType(MediaType.APPLICATION_JSON_VALUE)
-          .pathParam("id", right.getId())
-          .when()
-          .delete(ID_URL)
-          .then()
-          .statusCode(404).extract().asString();
-
-    assertThat(RAML_ASSERT_MESSAGE, restAssured.getLastReport(), RamlMatchers.hasNoViolations());
-  }
-
-  @Test
-  public void shouldUpdateRight() {
-
-    right.setDescription(DESCRIPTION);
-
-    Right response = restAssured.given()
-          .queryParam(ACCESS_TOKEN, getToken())
-          .contentType(MediaType.APPLICATION_JSON_VALUE)
-          .pathParam("id", right.getId())
-          .body(right)
-          .when()
-          .put(ID_URL)
-          .then()
-          .statusCode(200)
-          .extract().as(Right.class);
-
-    assertEquals(response.getDescription(), DESCRIPTION);
-    assertThat(RAML_ASSERT_MESSAGE, restAssured.getLastReport(), RamlMatchers.hasNoViolations());
-  }
-
-  @Test
-  public void shouldCreateNewRightIfDoesNotExist() {
-
-    rightRepository.delete(right);
-    right.setDescription(DESCRIPTION);
-
-    Right response = restAssured.given()
-          .queryParam(ACCESS_TOKEN, getToken())
-          .contentType(MediaType.APPLICATION_JSON_VALUE)
-          .pathParam("id", ID)
-          .body(right)
-          .when()
-          .put(ID_URL)
-          .then()
-          .statusCode(200)
-          .extract().as(Right.class);
-
-    assertEquals(response.getDescription(), DESCRIPTION);
-    assertThat(RAML_ASSERT_MESSAGE, restAssured.getLastReport(), RamlMatchers.hasNoViolations());
+  @After
+  public void cleanUp() {
+    rightRepository.deleteAll();
   }
 
   @Test
   public void shouldGetAllRights() {
 
-    Right[] response = restAssured.given()
-          .queryParam(ACCESS_TOKEN, getToken())
-          .contentType(MediaType.APPLICATION_JSON_VALUE)
-          .when()
-          .get(RESOURCE_URL)
-          .then()
-          .statusCode(200)
-          .extract().as(Right[].class);
+    RightDto[] response = restAssured
+        .given()
+        .queryParam(ACCESS_TOKEN, getToken())
+        .when()
+        .get(RESOURCE_URL)
+        .then()
+        .statusCode(200)
+        .extract().as(RightDto[].class);
 
-    Iterable<Right> rights = Arrays.asList(response);
-    assertTrue(rights.iterator().hasNext());
+    List<RightDto> rights = Arrays.asList(response);
+    assertThat(rights.size(), is(2));
     assertThat(RAML_ASSERT_MESSAGE, restAssured.getLastReport(), RamlMatchers.hasNoViolations());
   }
 
-  @Ignore
   @Test
-  public void shouldGetChosenRight() {
+  public void shouldGetRight() {
 
-    Right response = restAssured.given()
-          .queryParam(ACCESS_TOKEN, getToken())
-          .contentType(MediaType.APPLICATION_JSON_VALUE)
-          .pathParam("id", right.getId())
-          .when()
-          .get(ID_URL)
-          .then()
-          .statusCode(200)
-          .extract().as(Right.class);
+    RightDto response = restAssured
+        .given()
+        .queryParam(ACCESS_TOKEN, getToken())
+        .pathParam("id", right.getId())
+        .when()
+        .get(ID_URL)
+        .then()
+        .statusCode(200)
+        .extract().as(RightDto.class);
 
-    assertTrue(rightRepository.exists(response.getId()));
+    assertEquals(RIGHT_NAME, response.getName());
+    assertEquals(RightType.GENERAL_ADMIN, response.getType());
     assertThat(RAML_ASSERT_MESSAGE, restAssured.getLastReport(), RamlMatchers.hasNoViolations());
   }
 
-  @Ignore
   @Test
-  public void shouldNotGetNonexistentRight() {
+  public void shouldNotGetNonExistingRight() {
 
     rightRepository.delete(right);
 
-    restAssured.given()
-          .queryParam(ACCESS_TOKEN, getToken())
-          .contentType(MediaType.APPLICATION_JSON_VALUE)
-          .pathParam("id", right.getId())
-          .when()
-          .get(ID_URL)
-          .then()
-          .statusCode(404);
+    restAssured
+        .given()
+        .queryParam(ACCESS_TOKEN, getToken())
+        .pathParam("id", right.getId())
+        .when()
+        .get(ID_URL)
+        .then()
+        .statusCode(404);
+
+    assertThat(RAML_ASSERT_MESSAGE, restAssured.getLastReport(), RamlMatchers.hasNoViolations());
+  }
+
+  @Test
+  public void shouldCreateNewRight() {
+
+    rightRepository.delete(right);
+
+    RightDto response = restAssured
+        .given()
+        .queryParam(ACCESS_TOKEN, getToken())
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .body(rightDto)
+        .when()
+        .put(RESOURCE_URL)
+        .then()
+        .statusCode(200)
+        .extract().as(RightDto.class);
+
+    assertTrue(rightRepository.exists(response.getId()));
+    assertEquals(RIGHT_NAME, response.getName());
+    assertEquals(RightType.GENERAL_ADMIN, response.getType());
+    assertThat(RAML_ASSERT_MESSAGE, restAssured.getLastReport(), RamlMatchers.hasNoViolations());
+  }
+
+  @Test
+  public void shouldUpdateExistingRight() {
+
+    rightDto.setDescription(DESCRIPTION);
+
+    RightDto response = restAssured
+        .given()
+        .queryParam(ACCESS_TOKEN, getToken())
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .body(rightDto)
+        .when()
+        .put(RESOURCE_URL)
+        .then()
+        .statusCode(200)
+        .extract().as(RightDto.class);
+
+    assertEquals(RIGHT_NAME, response.getName());
+    assertEquals(RightType.GENERAL_ADMIN, response.getType());
+    assertEquals(DESCRIPTION, response.getDescription());
+    assertThat(RAML_ASSERT_MESSAGE, restAssured.getLastReport(), RamlMatchers.hasNoViolations());
+  }
+
+  @Test
+  public void shouldDeleteRight() {
+
+    restAssured
+        .given()
+        .queryParam(ACCESS_TOKEN, getToken())
+        .pathParam("id", right.getId())
+        .when()
+        .delete(ID_URL)
+        .then()
+        .statusCode(204);
+
+    assertFalse(rightRepository.exists(right.getId()));
+    assertThat(RAML_ASSERT_MESSAGE, restAssured.getLastReport(), RamlMatchers.hasNoViolations());
+  }
+
+  @Test
+  public void shouldNotDeleteNonExistingRight() {
+
+    rightRepository.delete(right);
+
+    restAssured
+        .given()
+        .queryParam(ACCESS_TOKEN, getToken())
+        .pathParam("id", right.getId())
+        .when()
+        .delete(ID_URL)
+        .then()
+        .statusCode(404);
 
     assertThat(RAML_ASSERT_MESSAGE, restAssured.getLastReport(), RamlMatchers.hasNoViolations());
   }
