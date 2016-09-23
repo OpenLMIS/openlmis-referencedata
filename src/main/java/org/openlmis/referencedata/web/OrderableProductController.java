4d1f57c9b72c84075fdea22f53ad34208b639091
Weronika Ciecierska <wciecierska@soldevelo.com>
2016-09-23 16:27:20 +0200

    OLMIS-763: Added endpoint to retrieve OrderableProduct with chosen id

--- src/main/java/org/openlmis/referencedata/web/OrderableProductController.java
+++ src/main/java/org/openlmis/referencedata/web/OrderableProductController.java
@@ -3,14 +3,20 @@ package org.openlmis.referencedata.web;
 import org.openlmis.referencedata.domain.OrderableProduct;
 import org.openlmis.referencedata.repository.OrderableProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
-public class OrderableProductController {
+public class OrderableProductController extends BaseController {
+
   @Autowired
   private OrderableProductRepository repository;
 
@@ -27,4 +33,20 @@ public class OrderableProductController {
 
     return allProducts;
   }
+
+  /**
+   * Finds product with chosen id.
+   * @param productId id of the chosen product
+   * @return chosen product
+   */
+  @RequestMapping(value = "/orderableProducts/{id}", method = RequestMethod.GET)
+  public ResponseEntity<?> getChosenOrderableProduct(@PathVariable("id") UUID productId) {
+    OrderableProduct product = repository.findOne(productId);
+    if (product == null) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    } else {
+      return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+  }
+
 }
