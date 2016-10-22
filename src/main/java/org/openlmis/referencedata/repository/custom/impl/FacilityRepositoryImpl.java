80855332afedab7af29cdb5bb5c1faf3406a9650
Pawel Muchowski <pmuchowski@soldevelo.com>
2016-10-22 20:58:14 +0200

    OLMIS-988: Fix search faclility by code or name

--- src/main/java/org/openlmis/referencedata/repository/custom/impl/FacilityRepositoryImpl.java
+++ src/main/java/org/openlmis/referencedata/repository/custom/impl/FacilityRepositoryImpl.java
@@ -1,15 +1,16 @@
 package org.openlmis.referencedata.repository.custom.impl;
 
+import org.openlmis.referencedata.domain.Facility;
+import org.openlmis.referencedata.repository.custom.FacilityRepositoryCustom;
+
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import org.openlmis.referencedata.domain.Facility;
-import org.openlmis.referencedata.repository.custom.FacilityRepositoryCustom;
-
-import java.util.List;
 
 public class FacilityRepositoryImpl implements FacilityRepositoryCustom {
 
@@ -30,16 +31,16 @@ public class FacilityRepositoryImpl implements FacilityRepositoryCustom {
     CriteriaBuilder builder = entityManager.getCriteriaBuilder();
     CriteriaQuery<Facility> query = builder.createQuery(Facility.class);
     Root<Facility> root = query.from(Facility.class);
-    Predicate predicate = builder.conjunction();
+    Predicate predicate = builder.disjunction();
+
     if (code != null) {
-      Predicate codeOrNamePredicate = builder.or(
-          builder.like(
-              root.get("code"), "%" + code + "%"),
-          builder.like(
-              root.get("name"), "%" + name + "%"));
-      predicate = builder.and(
-          predicate, codeOrNamePredicate);
+      predicate = builder.or(predicate, builder.like(root.get("code"), "%" + code + "%"));
     }
+
+    if (name != null) {
+      predicate = builder.or(predicate, builder.like(root.get("name"), "%" + name + "%"));
+    }
+
     query.where(predicate);
     return entityManager.createQuery(query).getResultList();
   }
