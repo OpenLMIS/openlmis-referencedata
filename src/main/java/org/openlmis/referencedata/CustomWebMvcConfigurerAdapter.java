74cddc1b65dfe27d3277739ae711731e8c1b4cfb
Jakub Kondrat <jkondrat@soldevelo.com>
2016-11-07 15:22:40 +0100

    OLMIS-1138 Fixing redirect on openlmis-blue

--- src/main/java/org/openlmis/referencedata/CustomWebMvcConfigurerAdapter.java
+++ src/main/java/org/openlmis/referencedata/CustomWebMvcConfigurerAdapter.java
@@ -1,5 +1,6 @@
 package org.openlmis.referencedata;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -7,9 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class CustomWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
 
+  @Value("${server.url}")
+  private String serverUrl;
+
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addViewController("/docs").setViewName("redirect:/docs/");
+    registry.addViewController("/docs").setViewName("redirect:" + serverUrl + "/docs/");
     registry.addViewController("/docs/").setViewName("forward:/docs/index.html");
     super.addViewControllers(registry);
   }
