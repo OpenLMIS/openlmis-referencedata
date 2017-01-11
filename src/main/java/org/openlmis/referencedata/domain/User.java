be2c3594632039dd7455f8cd01a10c7c663d109b
Jakub Kondrat <jkondrat@soldevelo.com>
2017-01-11 16:10:33 +0100

    OLMIS-969: Moving more classes to openlmis-service-util

--- src/main/java/org/openlmis/referencedata/domain/User.java
+++ src/main/java/org/openlmis/referencedata/domain/User.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import org.openlmis.referencedata.util.View;
+import org.openlmis.util.View;
 
 import java.util.Arrays;
 import java.util.HashSet;
--- src/main/java/org/openlmis/referencedata/util/View.java
+++
@@ -1,5 +0,0 @@
-package org.openlmis.referencedata.util;
-
-public class View {
-  public static class BasicInformation {}
-}
