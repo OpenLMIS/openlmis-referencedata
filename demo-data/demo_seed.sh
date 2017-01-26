a52b2f119c61c1ca35011653fbdf884c82bc6cbf
Chongsun Ahn <cahn922@yahoo.com>
2017-01-25 20:56:28 -0800

    OLMIS-1560 Add support for starter and demo data
    Starter data SQL file would be present in db/starter. Demo data script should use afterMigrate.sql file name instead of data.sql because that's what Flyway uses to run SQL files after migration scripts.
    
    Additionally add a conditional at the beginning of the demo data script to check for the existence of a table, which is created at the end of the script. This is so the script only runs once, the first time the service is deployed. These additional SQL statements are in the demo header and footer files.
    
    Add a line to prepend the header and the starter data script, as well as append the footer to the demo data script. The starter data script is embedded into the demo data script because Flyway can only run one script with the name afterMigrate.sql.

--- demo-data/demo_seed.sh
+++ demo-data/demo_seed.sh
@@ -14,9 +14,14 @@ FILES=`find ${DIRECTORY} -name "*.json"`
 # Run database input generation
 ${GENERATOR} ${FILES}
 
-mkdir ${OUTPUT_DIR}
-mv input.sql ${OUTPUT_DIR}/data.sql
+# Prepend and append conditional return so SQL file only runs once
+# Also prepend starter SQL file because Flyway only likes one afterMigrate.sql file
+cat ${DIRECTORY}/demo_sql_header.txt ${DIRECTORY}/../src/main/resources/db/starter/afterMigrate.sql input.sql ${DIRECTORY}/demo_sql_footer.txt > result.sql
 
-echo "Generated ${OUTPUT_DIR}/data.sql"
+mkdir ${OUTPUT_DIR}
+mv result.sql ${OUTPUT_DIR}/afterMigrate.sql
+rm input.sql
+
+echo "Generated ${OUTPUT_DIR}/afterMigrate.sql"
 echo "To insert the data into database, first run the service, and then from outside of container type:"
-echo "docker exec -i openlmisreferencedata_db_1 psql -Upostgres open_lmis < ${OUTPUT_DIR}/data.sql"
+echo "docker exec -i openlmisreferencedata_db_1 psql -Upostgres open_lmis < ${OUTPUT_DIR}/afterMigrate.sql"
