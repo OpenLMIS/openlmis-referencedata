3c831682e786ffc0ac9dd241481ae692ad6343d5
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-02-06 08:58:21 +0100

    OLMIS-1665: Removed REQUISITION_CONVERT_TO_ORDER right

--- src/main/resources/bootstrap.sql
+++ src/main/resources/bootstrap.sql
@@ -14,7 +14,6 @@ INSERT INTO referencedata.rights (id, name, type) VALUES ('bffa2de2-dc2a-47dd-b1
 INSERT INTO referencedata.rights (id, name, type) VALUES ('feb4c0b8-f6d2-4289-b29d-811c1d0b2863', 'REQUISITION_AUTHORIZE', 'SUPERVISION');
 INSERT INTO referencedata.rights (id, name, type) VALUES ('c3eb5df0-c3ac-4e70-a978-02827462f60e', 'REQUISITION_DELETE', 'SUPERVISION');
 INSERT INTO referencedata.rights (id, name, type) VALUES ('e101d2b8-6a0f-4af6-a5de-a9576b4ebc50', 'REQUISITION_VIEW', 'SUPERVISION');
-INSERT INTO referencedata.rights (id, name, type) VALUES ('7958129d-c4c0-4294-a40c-c2b07cb8e515', 'REQUISITION_CONVERT_TO_ORDER', 'ORDER_FULFILLMENT');
 INSERT INTO referencedata.rights (id, name, type) VALUES ('65626c3d-513f-4255-93fd-808709860594', 'FULFILLMENT_TRANSFER_ORDER', 'ORDER_FULFILLMENT');
 INSERT INTO referencedata.rights (id, name, type) VALUES ('24df2715-850c-4336-b650-90eb78c544bf', 'PODS_MANAGE', 'ORDER_FULFILLMENT');
 INSERT INTO referencedata.rights (id, name, type) VALUES ('35cf256f-1b28-41ad-84f5-99c158083dfc', 'REPORTS_VIEW', 'REPORTS');
