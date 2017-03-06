a556d573d4b617e731fcc125a59abcbfedbf6ce2
Yi Deng <dydyistc@gmail.com>
2017-03-06 17:54:58 +0800

    OLMIS-632 add new right to admin to manage organizations

--- src/main/resources/db/migration/20170206205310272__initial_bootstrap_data.sql
+++ src/main/resources/db/migration/20170206205310272__initial_bootstrap_data.sql
@@ -109,6 +109,7 @@ INSERT INTO rights (id, description, name, type) VALUES ('a25803c0-1ee7-4595-8e6
 INSERT INTO rights (id, description, name, type) VALUES ('69e50f15-2124-461a-9c8f-0f964fe7197b', NULL, 'STOCK_DESTINATIONS_VIEW', 'GENERAL_ADMIN');
 INSERT INTO rights (id, description, name, type) VALUES ('8973115f-81f7-4020-931c-a62209c2a963', NULL, 'STOCK_CARD_LINE_ITEM_REASONS_VIEW', 'GENERAL_ADMIN');
 INSERT INTO rights (id, description, name, type) VALUES ('17651f6d-39f3-41b6-adc8-4e3d901ac42c', NULL, 'STOCK_CARD_LINE_ITEM_REASONS_MANAGE', 'GENERAL_ADMIN');
+INSERT INTO rights (id, description, name, type) VALUES ('f76125b6-87e3-491d-aa5c-6f61b3cd88b7', NULL, 'ORGANIZATIONS_MANAGE', 'GENERAL_ADMIN');
 
 INSERT INTO rights (id, description, name, type) VALUES ('ebad51c3-f7c3-4fab-97e1-839973b045d4', NULL, 'USER_ROLES_MANAGE', 'GENERAL_ADMIN');
 INSERT INTO rights (id, description, name, type) VALUES ('42791f7a-84a1-470b-bc3c-4160bc99f13c', NULL, 'PROCESSING_SCHEDULES_MANAGE', 'GENERAL_ADMIN');
@@ -172,6 +173,7 @@ INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-1
 INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-11e6-80f5-76304dec7eb7', '69e50f15-2124-461a-9c8f-0f964fe7197b');
 INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-11e6-80f5-76304dec7eb7', '8973115f-81f7-4020-931c-a62209c2a963');
 INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-11e6-80f5-76304dec7eb7', '17651f6d-39f3-41b6-adc8-4e3d901ac42c');
+INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-11e6-80f5-76304dec7eb7', 'f76125b6-87e3-491d-aa5c-6f61b3cd88b7');
 
 INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-11e6-80f5-76304dec7eb7', '4e731cf7-854f-4af7-9ea4-bd5d8ed7bb22');
 INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-11e6-80f5-76304dec7eb7', 'ebad51c3-f7c3-4fab-97e1-839973b045d4');
