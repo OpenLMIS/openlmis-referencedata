1b2ae2459c6c9566c55fd75341aef5d83ac85db4
przemyslawStudzinskiSolDevelo <pstudzinski@soldevelo.com>
2017-03-22 15:12:46 +0100

    OLMIS-2143: Added new endnpoint for search users with pagination
    Is a temporary endpoint for search users with pagination,
    to not block UI development.
    We cannot simply add pagination to existing endpoint
    because is a "breaking change".
    Updated Raml.

--- src/main/java/org/openlmis/referencedata/web/UserController.java
+++ src/main/java/org/openlmis/referencedata/web/UserController.java
@@ -19,7 +19,7 @@ import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 
 import com.google.common.collect.Sets;
-
+import lombok.NoArgsConstructor;
 import org.openlmis.referencedata.domain.BaseEntity;
 import org.openlmis.referencedata.domain.Code;
 import org.openlmis.referencedata.domain.DirectRoleAssignment;
@@ -51,6 +51,7 @@ import org.openlmis.referencedata.repository.SupervisoryNodeRepository;
 import org.openlmis.referencedata.repository.UserRepository;
 import org.openlmis.referencedata.service.UserService;
 import org.openlmis.referencedata.util.Message;
+import org.openlmis.referencedata.util.Pagination;
 import org.openlmis.referencedata.util.messagekeys.ProgramMessageKeys;
 import org.openlmis.referencedata.util.messagekeys.RightMessageKeys;
 import org.openlmis.referencedata.util.messagekeys.UserMessageKeys;
@@ -59,6 +60,8 @@ import org.openlmis.util.PasswordResetRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -76,8 +79,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import lombok.NoArgsConstructor;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -289,6 +290,25 @@ public class UserController extends BaseController {
     return exportUsersToDtos(result);
   }
 
+  /**
+   * Returns all users with matched parameters and with pagination.
+   *
+   * @param queryMap request parameters (username, firstName, lastName, homeFacility, active,
+   *                 verified, loginRestricted) and JSON extraData.
+   * @return a list of all Users matching provided parameters.
+   */
+  @RequestMapping(value = "/users/search/page", method = RequestMethod.POST)
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public Page<UserDto> searchUsersWithPagination(
+      @RequestBody Map<String, Object> queryMap, Pageable pageable) {
+    rightService.checkAdminRight(RightName.USERS_MANAGE_RIGHT);
+
+    List<User> result = userService.searchUsers(queryMap);
+    List<UserDto> dtoList = exportUsersToDtos(result);
+
+    return Pagination.getPage(dtoList, pageable);
+  }
 
   /**
    * Check if user has a right with certain criteria.
