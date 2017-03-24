baf932b9b3d8a56273093f1a3f5ad572c9632e91
pfcui <cuipengfei2008@gmail.com>
2017-03-24 14:42:47 +0800

    [stock permissions] fix typo in uuid

--- src/main/resources/db/migration/20170206205310272__initial_bootstrap_data.sql
+++ src/main/resources/db/migration/20170206205310272__initial_bootstrap_data.sql
@@ -134,7 +134,7 @@ INSERT INTO rights (id, description, name, type) VALUES ('7b2da074-b754-4e49-bad
 INSERT INTO rights (id, description, name, type) VALUES ('60580166-ab1c-464a-8401-08384efc57b7', NULL, 'ORDERS_EDIT', 'ORDER_FULFILLMENT');
 
 INSERT INTO rights (id, description, name, type) VALUES ('94ae9da9-3d66-465e-9b1f-572c705e5d7f', NULL, 'STOCK_EVENT_CREATE', 'SUPERVISION');
-INSERT INTO rights (id, description, name, type) VALUES ('6831dad9-50fb-4f66-9dec-9861a053082', NULL, 'STOCK_INVENTORIES_EDIT', 'SUPERVISION');
+INSERT INTO rights (id, description, name, type) VALUES ('6fb013fe-d878-43e9-bff0-fa5431e62c34', NULL, 'STOCK_INVENTORIES_EDIT', 'SUPERVISION');
 INSERT INTO rights (id, description, name, type) VALUES ('7b41c10e-5489-47a9-8a68-69ae74b8a4cf', NULL, 'STOCK_ADJUST', 'SUPERVISION');
 
 
