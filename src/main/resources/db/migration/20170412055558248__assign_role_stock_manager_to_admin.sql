36e6f916f07a3ee9a2f80cde999a4e53afef3b77
Yi Deng <dydyistc@gmail.com>
2017-04-12 15:03:36 +0800

    OLMIS-2098 assign role stock manager to user administrator

---
+++ src/main/resources/db/migration/20170412055558248__assign_role_stock_manager_to_admin.sql
@@ -0,0 +1,8 @@
+--
+-- Data for Name: role_assignments; Type: TABLE DATA; Schema: referencedata; Owner: postgres
+--
+
+INSERT INTO referencedata.role_assignments (id, type, supervisorynodeid, programid, userid, roleid) VALUES ('9510798a-a0e4-43fe-8b85-9e8e34623121', 'supervision', 'fb38bd1c-beeb-4527-8345-900900329c10', '10845cb9-d365-4aaa-badd-b4fa39c6a26a', 'a337ec45-31a0-4f2b-9b2e-a105c4b669bb', 'd8ff49b1-96e7-4710-9511-289f490f7577');
+INSERT INTO referencedata.role_assignments (id, type, supervisorynodeid, programid, userid, roleid) VALUES ('e4d863f2-e5b8-469d-833f-5d1c72753f7d', 'supervision', 'fb38bd1c-beeb-4527-8345-900900329c10', 'dce17f2e-af3e-40ad-8e00-3496adef44c3', 'a337ec45-31a0-4f2b-9b2e-a105c4b669bb', 'd8ff49b1-96e7-4710-9511-289f490f7577');
+INSERT INTO referencedata.role_assignments (id, type, supervisorynodeid, programid, userid, roleid) VALUES ('14129b04-bf61-40a1-a626-acec3db6a38f', 'supervision', '6d5831e7-c336-4129-bd10-0149910a72aa', '10845cb9-d365-4aaa-badd-b4fa39c6a26a', 'a337ec45-31a0-4f2b-9b2e-a105c4b669bb', 'd8ff49b1-96e7-4710-9511-289f490f7577');
+INSERT INTO referencedata.role_assignments (id, type, supervisorynodeid, programid, userid, roleid) VALUES ('8a30683a-29f6-4abf-9391-3d2c806ea3a3', 'supervision', '6d5831e7-c336-4129-bd10-0149910a72aa', 'dce17f2e-af3e-40ad-8e00-3496adef44c3', 'a337ec45-31a0-4f2b-9b2e-a105c4b669bb', 'd8ff49b1-96e7-4710-9511-289f490f7577');
