543534ecaccc10e63a98b2290f019a6a605fc29d
Paweł Albecki <palbecki@soldevelo.com>
2017-05-09 10:50:14 +0200

    OLMIS-2479: fix ftap migration

--- src/main/resources/db/migration/20170414131122839__refactor_ftap.sql
+++ src/main/resources/db/migration/20170414131122839__refactor_ftap.sql
@@ -1,4 +1,15 @@
 ALTER TABLE facility_type_approved_products
-DROP COLUMN programorderableid,
-ADD COLUMN orderableid UUID NOT NULL,
-ADD COLUMN programid UUID NOT NULL;
+ADD COLUMN orderableid UUID,
+ADD COLUMN programid UUID;
+
+UPDATE facility_type_approved_products
+SET orderableid = program_orderables.orderableid,
+programid = program_orderables.programid
+FROM program_orderables
+WHERE facility_type_approved_products.programorderableid = program_orderables.id;
+
+ALTER TABLE facility_type_approved_products
+DROP COLUMN programorderableid;
+
+ALTER TABLE facility_type_approved_products ALTER COLUMN orderableid SET NOT NULL;
+ALTER TABLE facility_type_approved_products ALTER COLUMN programid SET NOT NULL;
