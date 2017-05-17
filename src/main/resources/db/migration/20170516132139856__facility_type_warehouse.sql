a2d030f67f7d1069029fd687081ebbbc0a87c994
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-05-17 11:55:18 +0200

    OLMIS-2492: fixed migration and updated error message

--- src/main/resources/db/migration/20170516132139856__facility_type_warehouse.sql
+++ src/main/resources/db/migration/20170516132139856__facility_type_warehouse.sql
@@ -1,5 +1,6 @@
-DELETE FROM facility_types
-    WHERE code = 'warehouse';
-
 INSERT INTO facility_types (id, code, name, active, displayOrder)
-    VALUES ('e2faaa9e-4b2d-4212-bb60-fd62970b2113', 'warehouse', 'Warehouse', true, 1);
+    SELECT 'e2faaa9e-4b2d-4212-bb60-fd62970b2113', 'warehouse', 'Warehouse', true, 1
+WHERE
+    NOT EXISTS (
+        SELECT code FROM facility_types WHERE code = 'warehouse'
+    );
