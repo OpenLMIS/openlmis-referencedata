46f7b54cdd2d92dab64b9245efeff705ab0c8f11
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-05-17 10:59:58 +0200

    Revert "OLMIS-2492: fixed inserting warehouse in migration"
    This reverts commit df04d013faea38f384612e27c8ea4b8853325ae5.

--- src/main/resources/db/migration/20170516132139856__facility_type_warehouse.sql
+++ src/main/resources/db/migration/20170516132139856__facility_type_warehouse.sql
@@ -1,3 +1,5 @@
+DELETE FROM facility_types
+    WHERE code = 'warehouse';
+
 INSERT INTO facility_types (id, code, name, active, displayOrder)
-    VALUES ('e2faaa9e-4b2d-4212-bb60-fd62970b2113', 'warehouse', 'Warehouse', true, 1)
-        ON CONFLICT (code) DO NOTHING;
+    VALUES ('e2faaa9e-4b2d-4212-bb60-fd62970b2113', 'warehouse', 'Warehouse', true, 1);
