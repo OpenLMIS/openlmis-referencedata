eb0b63450f0accc450464ccb097b9568922a343d
Paweł Albecki <palbecki@soldevelo.com>
2017-07-10 14:14:24 +0200

    OLMIS-2609: Create rights to manage CCE
    assign to system administrator

---
+++ src/main/resources/db/migration/20170710120137838__create_cce-manage_right.sql
@@ -0,0 +1,3 @@
+INSERT INTO rights (id, description, name, type) VALUES ('75063c92-8987-4624-9138-e8f163546541', NULL, 'CCE_MANAGE', 'GENERAL_ADMIN');
+
+INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-11e6-80f5-76304dec7eb7', '75063c92-8987-4624-9138-e8f163546541');
