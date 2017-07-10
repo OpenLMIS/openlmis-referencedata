7a50669e19420bad2711fb8e4339b3e8e8825dad
joshzamor <josh.zamor@villagereach.org>
2017-07-10 12:44:02 -0700

    OLMIS-2812, allow the base URL to be configurable in performance testing

--- performance/test.sh
+++ performance/test.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env sh
 
-docker run --rm -v $(pwd):/bzt-configs \
+# optional first argument is a base URL for the endpoints to be tested
+# remember to quote the URL, so for example:
+# ./test.sh 'http://localhost'
+
+# determine base uri from first argument or use default
+URL='https://test.openlmis.org'
+if [ ! -z "'$1'" ]; then
+  URL=$1
+fi
+
+echo "Running performance tests against: $URL"
+export BASE_URL="${URL}"
+docker run --rm -e BASE_URL -v $(pwd):/bzt-configs \
   -v $(pwd)/../build/performance-artifacts:/tmp/artifacts \
   undera/taurus \
-  -o modules.jmeter.properties.base-uri='https://test.openlmis.org' \
+  -o modules.jmeter.properties.base-uri="${BASE_URL}" \
   -o reporting.2.dump-xml=/tmp/artifacts/stats.xml \
   config.yml \
   tests/*.yml
