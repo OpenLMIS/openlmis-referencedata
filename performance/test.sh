7b0c7b3ef9b0c44d7631bd05472a050a3e6361b4
joshzamor <josh.zamor@villagereach.org>
2017-07-10 12:16:35 -0700

    OLMIS-2812, move performance results to build, add final stats output

--- performance/test.sh
+++ performance/test.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-rm -Rf artifacts/*
-docker run --rm -v $(pwd):/bzt-configs -v $(pwd)/artifacts:/tmp/artifacts \
+docker run --rm -v $(pwd):/bzt-configs \
+  -v $(pwd)/../build/performance-artifacts:/tmp/artifacts \
   undera/taurus \
   -o modules.jmeter.properties.base-uri='https://test.openlmis.org' \
+  -o reporting.2.dump-xml=/tmp/artifacts/stats.xml \
   config.yml \
   tests/*.yml
