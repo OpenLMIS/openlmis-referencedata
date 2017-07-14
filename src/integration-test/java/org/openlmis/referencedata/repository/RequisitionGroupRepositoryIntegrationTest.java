a5c89548d2fafcc602c5cf85a67a84ca4be0c4a8
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-07-14 14:24:45 +0200

    OLMIS-2696: removed unused import

--- src/integration-test/java/org/openlmis/referencedata/repository/RequisitionGroupRepositoryIntegrationTest.java
+++ src/integration-test/java/org/openlmis/referencedata/repository/RequisitionGroupRepositoryIntegrationTest.java
@@ -38,7 +38,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
