b675c2991026137e07ce03dd167321c1c8119715
Paweł Gesek <pgesek@soldevelo.com>
2017-07-14 16:52:41 +0200

    Revert "Revert "Revert "add spring boot health check"""
    This reverts commit fe0cfefe896c8d8920140e6801a6cccb0b3c80e9.

--- src/main/java/org/openlmis/referencedata/security/ResourceServerSecurityConfiguration.java
+++ src/main/java/org/openlmis/referencedata/security/ResourceServerSecurityConfiguration.java
@@ -82,7 +82,6 @@ public class ResourceServerSecurityConfiguration implements ResourceServerConfig
             "/referencedata",
             "/webjars/**",
             "/referencedata/webjars/**",
-            "/health",
             "/referencedata/docs/**"
         ).permitAll()
         .antMatchers("/**").fullyAuthenticated();
