66fff293f16961ece48300a1de9386fc37e0970b
Paweł Nawrocki <pnawrocki@soldevelo.com>
2017-07-20 15:50:10 +0200

    OLMIS-2610: Created rights to manage CCE Inventory, added demo data with sample role assignments

---
+++ src/main/resources/db/migration/20170720131319162__create_cce_inventory_manage_rights.sql
@@ -0,0 +1,2 @@
+INSERT INTO rights (id, description, name, type) VALUES ('9440fbe1-fd9b-4e9a-abb9-108263f92506', NULL, 'CCE_INVENTORY_VIEW', 'SUPERVISION');
+INSERT INTO rights (id, description, name, type) VALUES ('cf00abaf-bca5-49c7-a201-23b066374a99', NULL, 'CCE_INVENTORY_EDIT', 'SUPERVISION');
