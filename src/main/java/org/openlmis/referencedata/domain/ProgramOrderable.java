e2d183f44e9bfb640561f196771df9fa325b169a
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-08-07 13:56:18 +0200

    MW-129: Fixed NPE in ProgramOrderable.equals method

--- src/main/java/org/openlmis/referencedata/domain/ProgramOrderable.java
+++ src/main/java/org/openlmis/referencedata/domain/ProgramOrderable.java
@@ -165,7 +165,9 @@ public class ProgramOrderable extends BaseEntity {
     }
 
     ProgramOrderable otherProgProduct = (ProgramOrderable) other;
-    return program.equals(otherProgProduct.program) && product.equals(otherProgProduct.product);
+
+    return Objects.equals(program, otherProgProduct.program)
+        && Objects.equals(product, otherProgProduct.product);
   }
 
   @Override
