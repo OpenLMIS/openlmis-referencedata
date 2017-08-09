fac4da450d4dcf95df01d2866730c5ad10abcdad
joshzamor <josh.zamor@villagereach.org>
2017-08-09 16:46:23 -0700

    OLMIS-2813, add pageable utility class

---
+++ src/main/java/org/openlmis/referencedata/repository/custom/impl/PageableUtil.java
@@ -0,0 +1,38 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+package org.openlmis.referencedata.repository.custom.impl;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.data.domain.Pageable;
+
+class PageableUtil {
+
+  /**
+   * get {@link javax.persistence.Query}'s {@link javax.persistence.Query#setMaxResults(int)} and
+   * {@link javax.persistence.Query#setFirstResult(int)} from a {@link Pageable}.
+   * @param pageable the pageable that has {@link Pageable#getPageNumber()} and
+   * {@link Pageable#getPageSize()}.  If the pageable is null, then sensible defaults are returned.
+   * @return a pair where {@link Pair#getLeft()} is for
+   *   {@link javax.persistence.Query#setMaxResults(int)} and {@link Pair#getRight()} is for
+   *   {@link javax.persistence.Query#setFirstResult(int)}. 0,0 if Pageable is null.
+   */
+  static Pair<Integer, Integer> querysMaxAndFirstResult(Pageable pageable) {
+    int pageSize = null != pageable ? pageable.getPageSize() : 0;
+    int firstResult = null != pageable ? pageable.getPageNumber() * pageSize : 0;
+    return new ImmutablePair<>(pageSize, firstResult);
+  }
+}
---
+++ src/test/java/org/openlmis/referencedata/repository/custom/impl/PageableUtilTest.java
@@ -0,0 +1,56 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+package org.openlmis.referencedata.repository.custom.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Test;
+import org.springframework.data.domain.Pageable;
+
+public class PageableUtilTest {
+
+  @Test
+  public void querysMaxAndFirstResultShouldBeSafeWithNullPageable() {
+    // when
+    Pair maxAndFirst = PageableUtil.querysMaxAndFirstResult(null);
+
+    // then
+    assertEquals(0, maxAndFirst.getLeft());
+    assertEquals(0, maxAndFirst.getRight());
+  }
+
+  @Test
+  public void querysMaxAndFirstShouldUsePageable() {
+    // given
+    Pageable pageable = mock(Pageable.class);
+    when(pageable.getPageSize()).thenReturn(10);
+    when(pageable.getPageNumber()).thenReturn(2);
+
+    // when
+    Pair maxAndFirst = PageableUtil.querysMaxAndFirstResult(pageable);
+
+    // then
+    verify(pageable, times(1)).getPageSize();
+    verify(pageable, times(1)).getPageNumber();
+    assertEquals(10, maxAndFirst.getLeft());
+    assertEquals(20, maxAndFirst.getRight());
+  }
+}
