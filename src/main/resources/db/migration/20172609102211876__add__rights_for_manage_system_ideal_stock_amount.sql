587477695c2fae441af2c05d07205cffcc3ab482
Paweł Albecki <palbecki@soldevelo.com>
2017-09-25 10:34:07 +0200

    OLMIS-2966: add rights to manage ideal stock amount, assign to admin

---
+++ src/main/resources/db/migration/20172609102211876__add__rights_for_manage_system_ideal_stock_amount.sql
@@ -0,0 +1,5 @@
+INSERT INTO rights (id, description, name, type) VALUES ('3f4c6b74-5426-4646-925b-c1982648846c', NULL, 'SYSTEM_IDEAL_STOCK_AMOUNT_MANAGE', 'GENERAL_ADMIN');
+INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-11e6-80f5-76304dec7eb7', '3f4c6b74-5426-4646-925b-c1982648846c');
+
+INSERT INTO rights (id, description, name, type) VALUES ('dfdca052-c1f9-4d1b-9ad9-048377b97e12', NULL, 'IDEAL_STOCK_AMOUNT_VIEW', 'SUPERVISION');
+INSERT INTO rights (id, description, name, type) VALUES ('5cd8a7a8-50f7-4938-8936-cdb852b38455', NULL, 'IDEAL_STOCK_AMOUNT_EDIT', 'SUPERVISION');
