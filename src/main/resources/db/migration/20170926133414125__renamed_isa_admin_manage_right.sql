ef51e6810430322b59efaca75a93b5080be71bf8
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-09-26 15:02:59 +0200

    OLMIS-3227: fixed renaming ISA manage right migration

--- src/main/resources/db/migration/20170926133414125__renamed_isa_admin_manage_right.sql
+++ src/main/resources/db/migration/20170926133414125__renamed_isa_admin_manage_right.sql
@@ -1,7 +1,2 @@
-DELETE FROM role_rights
-    WHERE roleid = 'a439c5de-b8aa-11e6-80f5-76304dec7eb7' AND rightid = '3f4c6b74-5426-4646-925b-c1982648846c';
-DELETE FROM rights
+UPDATE rights SET name = 'SYSTEM_IDEAL_STOCK_AMOUNTS_MANAGE'
     WHERE id = '3f4c6b74-5426-4646-925b-c1982648846c';
-
-INSERT INTO rights (id, description, name, type) VALUES ('bc4111a2-14f2-42aa-8f97-d9502d611ac7', NULL, 'SYSTEM_IDEAL_STOCK_AMOUNTS_MANAGE', 'GENERAL_ADMIN');
-INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-11e6-80f5-76304dec7eb7', 'bc4111a2-14f2-42aa-8f97-d9502d611ac7');
