894493c72c73b572fd81d8db1ddd2122fd79a9d7
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-11-30 15:05:45 +0100

    OLMIS-3625: Added indexes for FTAP foreign keys

---
+++ src/main/resources/db/migration/20171130114919223__created_indexes_on_ftap.sql
@@ -0,0 +1,3 @@
+CREATE INDEX ON referencedata.facility_type_approved_products (orderableId);
+CREATE INDEX ON referencedata.facility_type_approved_products (programId);
+CREATE INDEX ON referencedata.facility_type_approved_products (facilityTypeId);
