75ef3ffeefe6a576879038c455b51d779235759f
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-12-04 13:30:45 +0100

    OLMIS-3625: Added indexes on program orderable fields

---
+++ src/main/resources/db/migration/20171204124230751__created_indexes_on_program_orderables.sql
@@ -0,0 +1,3 @@
+CREATE INDEX ON referencedata.program_orderables (programId);
+CREATE INDEX ON referencedata.program_orderables (orderableId);
+CREATE INDEX ON referencedata.program_orderables (orderableDisplayCategoryId);
