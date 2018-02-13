937c6dc0f4976e7f2e6cef94e5330035cfb3dba9
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2018-02-13 13:46:14 +0100

    OLMIS-4052: fixed default page size

--- src/main/java/org/openlmis/referencedata/CustomWebMvcConfigurerAdapter.java
+++ src/main/java/org/openlmis/referencedata/CustomWebMvcConfigurerAdapter.java
@@ -15,11 +15,17 @@
 
 package org.openlmis.referencedata;
 
+import org.openlmis.referencedata.util.Pagination;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
 public class CustomWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
@@ -41,5 +47,16 @@ public class CustomWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
     registry.addResourceHandler("/referencedata/webjars/**")
         .addResourceLocations("classpath:/META-INF/resources/webjars/");
     super.addResourceHandlers(registry);
+  }
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+    PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+    resolver.setMaxPageSize(Pagination.NO_PAGINATION);
+    resolver.setFallbackPageable(new PageRequest(
+            Pagination.DEFAULT_PAGE_NUMBER,
+            Pagination.NO_PAGINATION));
+    argumentResolvers.add(resolver);
+    super.addArgumentResolvers(argumentResolvers);
   }
 }
