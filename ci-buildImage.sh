c6a8af9c187c85918410d2b4d4a78f0e2899464b
joshzamor <josh.zamor@villagereach.org>
2018-06-22 12:54:26 +0300

    OLMIS-4800, remove duplicate volume removal

--- ci-buildImage.sh
+++ ci-buildImage.sh
@@ -16,7 +16,6 @@ fi
 
 docker-compose -f docker-compose.builder.yml run -e BUILD_NUMBER=$BUILD_NUMBER -e GIT_BRANCH=$GIT_BRANCH builder
 docker-compose -f docker-compose.builder.yml build image
-docker-compose -f docker-compose.builder.yml down --volumes
 docker tag openlmis/referencedata:latest openlmis/referencedata:$STAGING_VERSION
 docker push openlmis/referencedata:$STAGING_VERSION
 
