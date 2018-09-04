58a36f752be80db7bcc7556ff3aad3ebbab9be28
Łukasz Lewczyński <llewczynski@soldevelo.com>
2018-09-04 13:49:13 +0200

    OLMIS-4949: added test for Facility.updateFrom method

--- src/test/java/org/openlmis/referencedata/domain/FacilityTest.java
+++ src/test/java/org/openlmis/referencedata/domain/FacilityTest.java
@@ -16,11 +16,11 @@
 package org.openlmis.referencedata.domain;
 
 import static com.google.common.collect.Sets.newHashSet;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openlmis.referencedata.dto.FacilityDto;
 import org.openlmis.referencedata.testbuilder.FacilityDataBuilder;
 import org.openlmis.referencedata.testbuilder.ProgramDataBuilder;
 import org.openlmis.referencedata.testbuilder.SupportedProgramDataBuilder;
@@ -30,20 +30,20 @@ public class FacilityTest {
   private Program program;
 
   @Before
-  public void setUp() throws Exception {
+  public void setUp() {
     program = new ProgramDataBuilder().build();
     facility = new FacilityDataBuilder().withSupportedProgram(program).build();
   }
 
   @Test
-  public void supportsShouldReturnTrueIfSupportsProgram() throws Exception {
-    assertThat(facility.supports(program), is(true));
+  public void supportsShouldReturnTrueIfSupportsProgram() {
+    assertThat(facility.supports(program)).isTrue();
   }
 
   @Test
-  public void supportsShouldReturnFalseIfDoesNotSupportProgram() throws Exception {
+  public void supportsShouldReturnFalseIfDoesNotSupportProgram() {
     Program otherProgram = new ProgramDataBuilder().build();
-    assertThat(facility.supports(otherProgram), is(false));
+    assertThat(facility.supports(otherProgram)).isFalse();
   }
 
   @Test
@@ -53,6 +53,19 @@ public class FacilityTest {
         .withProgram(program)
         .buildAsInactive();
     facility.setSupportedPrograms(newHashSet(supportedProgram));
-    assertThat(facility.supports(program), is(false));
+    assertThat(facility.supports(program)).isFalse();
   }
+
+  @Test
+  public void shouldUpdateFromAnotherFacility() {
+    FacilityDto newVersion = new FacilityDto();
+    new FacilityDataBuilder().build().export(newVersion);
+
+    facility.updateFrom(newVersion);
+
+    assertThat(facility)
+        .isEqualToIgnoringGivenFields(newVersion,
+            "id", "geographicZone", "type", "operator", "supportedPrograms");
+  }
+
 }
