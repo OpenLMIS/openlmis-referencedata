fcf85efd3d4a78179837fe125370e166b07c6bf3
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2019-03-05 16:14:48 +0100

    OLMIS-6055: Added generated comment to the migration file

--- src/main/resources/db/migration/20190304135759678__add_rights_per_report.sql
+++ src/main/resources/db/migration/20190304135759678__add_rights_per_report.sql
@@ -1,3 +1,7 @@
+-- WHEN COMMITTING OR REVIEWING THIS FILE: Make sure that the timestamp in the file name (that serves as a version) is the latest timestamp, and that no new migration have been added in the meanwhile.
+-- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way.
+-- Migrations should NOT BE EDITED. Add a new migration to apply changes.
+
 INSERT INTO rights (id, description, name, type) VALUES ('4eaac390-1156-42c9-9fa0-83130c4519d4', NULL, 'REPORTING_RATE_AND_TIMELINESS_REPORT_VIEW', 'REPORTS');
 INSERT INTO rights (id, description, name, type) VALUES ('526e5ee5-b07c-4cc7-bc4b-3fa77bde0699', NULL, 'STOCK_STATUS_REPORT_VIEW', 'REPORTS');
 INSERT INTO rights (id, description, name, type) VALUES ('afdbf2a3-c099-40fa-93ea-1c18794e3cd4', NULL, 'STOCKOUTS_REPORT_VIEW', 'REPORTS');
