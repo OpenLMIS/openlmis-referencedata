9551b7bfe8bb4612f8f6688f3cc9c635f41be8b2
Paweł Cieszko <pcieszko@soldevelo.com>
2019-05-28 13:39:08 +0200

    OLMIS-6322: Added a new right for ADMINISTRATIVE_MESSAGES_MANAGE

---
+++ src/main/resources/db/migration/20190523163900000__added_administrative_messages_manage.sql
@@ -0,0 +1,3 @@
+INSERT INTO rights (id, description, name, type) VALUES ('75773629-f848-480f-83d5-973673ade29c', NULL, 'ADMINISTRATIVE_MESSAGES_MANAGE', 'GENERAL_ADMIN');
+
+INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-11e6-80f5-76304dec7eb7', '75773629-f848-480f-83d5-973673ade29c');
