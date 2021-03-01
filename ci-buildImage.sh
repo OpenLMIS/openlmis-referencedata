b247105f26a312b39c7ad3fdfd30008767ee0f3e
jakubjakubowskisoldevelo <jakubjakubowski@soldevelo.com>
2021-03-01 15:41:18 +0100

    SELV3-238 - update jenkins jar build files

--- ci-buildImage.sh
+++ ci-buildImage.sh
@@ -8,13 +8,17 @@ function finish {
 }
 trap finish EXIT
 
+sudo rm -f .signing/secring.gpg
 sudo rm -f .env
+mkdir .signing/
+cp $SECRING_FILE .signing/secring.gpg
 cp $ENV_FILE .env
 if [ "$GIT_BRANCH" != "master" ]; then
     sed -i '' -e "s#^TRANSIFEX_PUSH=.*#TRANSIFEX_PUSH=false#" .env  2>/dev/null || true
 fi
 
-docker-compose -f docker-compose.builder.yml run -e BUILD_NUMBER=$BUILD_NUMBER -e GIT_BRANCH=$GIT_BRANCH builder
+docker-compose -f docker-compose.builder.yml run -e BUILD_NUMBER=$BUILD_NUMBER -e GIT_BRANCH=$GIT_BRANCH \
+-e SIGNING_KEYID=$SIGNING_KEYID -e SIGNING_PASSWORD=$SIGNING_PASSWORD -e OSSRH_USERNAME=$OSSRH_USERNAME -e OSSRH_PASSWORD=$OSSRH_PASSWORD builder
 docker-compose -f docker-compose.builder.yml build image
 docker tag openlmis/referencedata:latest openlmis/referencedata:$STAGING_VERSION
 docker push openlmis/referencedata:$STAGING_VERSION
