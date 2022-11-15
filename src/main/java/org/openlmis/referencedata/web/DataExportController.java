4ff66a73a4cc6721e0f9c2f72017ce1fbfd9fa9c
Szymon Radziszewski <sradziszewski@soldevelo.com>
2022-11-15 16:44:14 +0100

    OLMIS-7643: Added RESOURCE_PATH constant and exportData method with javadoc comment

--- src/main/java/org/openlmis/referencedata/web/DataExportController.java
+++ src/main/java/org/openlmis/referencedata/web/DataExportController.java
@@ -1,7 +1,18 @@
 package org.openlmis.referencedata.web;
 
-public class DataExportController {
+import org.springframework.web.bind.annotation.RequestParam;
 
+public class DataExportController extends BaseController {
 
+  public static final String RESOURCE_PATH = API_PATH + "/exportData";
+
+  /**
+   * Export data to a file of a given format.
+   *
+   * @param data   The names of the files to be exported.
+   */
+  public void exportData(@RequestParam(value = "data", required = true) String data) {
+
+  }
 
 }
