ed7c8400feb7342bdf4958f1ba884af1c88dd1cf
Szymon Radziszewski <sradziszewski@soldevelo.com>
2022-11-15 16:46:14 +0100

    OLMIS-7643: Added annotations for DataExportController class

--- src/main/java/org/openlmis/referencedata/web/DataExportController.java
+++ src/main/java/org/openlmis/referencedata/web/DataExportController.java
@@ -1,7 +1,32 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
 package org.openlmis.referencedata.web;
 
-import org.springframework.web.bind.annotation.RequestParam;
+import static org.openlmis.referencedata.web.DataExportController.RESOURCE_PATH;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@Controller
+@RequestMapping(RESOURCE_PATH)
 public class DataExportController extends BaseController {
 
   public static final String RESOURCE_PATH = API_PATH + "/exportData";
@@ -11,6 +36,8 @@ public class DataExportController extends BaseController {
    *
    * @param data   The names of the files to be exported.
    */
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
   public void exportData(@RequestParam(value = "data", required = true) String data) {
 
   }
