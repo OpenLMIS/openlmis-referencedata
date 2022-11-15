b6185c0b17ba2bb52cc7b2f8b5f78f95957d7347
Szymon Radziszewski <sradziszewski@soldevelo.com>
2022-11-15 16:48:29 +0100

    OLMIS-7643: Added exportDataTest method to check the response status

--- src/test/java/org/openlmis/referencedata/web/DataExportControllerTest.java
+++ src/test/java/org/openlmis/referencedata/web/DataExportControllerTest.java
@@ -1,6 +1,43 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
 package org.openlmis.referencedata.web;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+@ExtendWith(SpringExtension.class)
 class DataExportControllerTest {
 
+  private MockMvc mockMvc;
+
+  @BeforeAll
+  void init() {
+    mockMvc = MockMvcBuilders.standaloneSetup(DataExportController.class).build();
+  }
+
+  @Test
+  void exportDataTest() throws Exception {
+    mockMvc.perform(get("*/exportData")).andExpect(status().isOk());
+  }
 }
