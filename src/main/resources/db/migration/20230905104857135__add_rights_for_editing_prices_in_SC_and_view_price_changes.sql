f08bc36d6838a7f530eeca6478592c6ef862f8d8
dariapionk <dpionk@soldevelo.com>
2023-09-05 13:02:32 +0200

    AO-800: Add new rights for managing product's price

---
+++ src/main/resources/db/migration/20230905104857135__add_rights_for_editing_prices_in_SC_and_view_price_changes.sql
@@ -0,0 +1,2 @@
+INSERT INTO rights (id, description, name, type) VALUES ('2ec4fee6-da48-4cbd-8c91-d55b827f4cc0', NULL, 'EDIT_PRODUCT_PRICE_STOCK_MANAGEMENT', 'GENERAL_ADMIN');
+INSERT INTO rights (id, description, name, type) VALUES ('a077fbba-b623-425c-a8ce-84f19bff0068', NULL, 'PRICE_CHANGES_VIEW', 'SUPERVISION');
