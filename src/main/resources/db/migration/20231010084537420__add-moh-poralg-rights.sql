d624e16f837609a8341e4ab57ddf74aa8c271936
kchelstowski <kchelstowski@soldevelo.com>
2023-10-10 15:39:49 +0200

    TZUP-611 Split MOH/PORALG rights

---
+++ src/main/resources/db/migration/20231010084537420__add-moh-poralg-rights.sql
@@ -0,0 +1,2 @@
+INSERT INTO rights (id, description, name, type) VALUES ('28827a19-6dd2-470c-a5f4-dddabdcb50e8', NULL, 'MOH_APPROVAL', 'SUPERVISION');
+INSERT INTO rights (id, description, name, type) VALUES ('56c5b03f-8df6-4cc9-89e1-122fd052be78', NULL, 'PORALG_APPROVAL', 'SUPERVISION');
