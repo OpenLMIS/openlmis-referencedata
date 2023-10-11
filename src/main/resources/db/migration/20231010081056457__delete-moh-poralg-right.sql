5a18ed6f6efb22ee03eab187eac08a6294a3e61b
kchelstowski <kchelstowski@soldevelo.com>
2023-10-11 13:42:02 +0200

    TZUP-611 Remove Moh right from right_assignments associated table

--- src/main/resources/db/migration/20231010081056457__delete-moh-poralg-right.sql
+++ src/main/resources/db/migration/20231010081056457__delete-moh-poralg-right.sql
@@ -1,4 +1,6 @@
 DELETE FROM referencedata.role_rights WHERE rightid = '0f41c043-d61c-44ad-837d-f7978efc8978';
 
+DELETE FROM referencedata.right_assignments WHERE rightname = 'MOH_PORALG_APPROVALS';
+
 DELETE FROM referencedata.rights WHERE id = '0f41c043-d61c-44ad-837d-f7978efc8978' or name = 'MOH_PORALG_APPROVALS';
 
