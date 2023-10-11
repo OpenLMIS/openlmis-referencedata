ff41d5220516b5ab9920140d578e13b4921ee7f3
Kacper Chełstowski <kchelstowski@soldevelo.com>
2023-10-11 11:04:00 +0200

    TZUP-611 Split MOH/PORALG rights (#71)

---
+++ src/main/resources/db/migration/20231010081056457__delete-moh-poralg-right.sql
@@ -0,0 +1,4 @@
+DELETE FROM referencedata.role_rights WHERE rightid = '0f41c043-d61c-44ad-837d-f7978efc8978';
+
+DELETE FROM referencedata.rights WHERE id = '0f41c043-d61c-44ad-837d-f7978efc8978' or name = 'MOH_PORALG_APPROVALS';
+
