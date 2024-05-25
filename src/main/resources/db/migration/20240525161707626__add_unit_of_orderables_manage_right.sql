99f4ccbbb0548b05cc2bbd301f5a347f687d02cb
tsznaj <tsznajderski@soldevelo.com>
2024-05-25 18:23:23 +0200

    OAM-91: Added rights for managing unit of orderables.

---
+++ src/main/resources/db/migration/20240525161707626__add_unit_of_orderables_manage_right.sql
@@ -0,0 +1,3 @@
+INSERT INTO rights (id, description, name, type) VALUES ('d9f9206d-e54e-43c4-89ff-bc67c8a2581f', NULL, 'UNIT_OF_ORDERABLES_MANAGE', 'GENERAL_ADMIN');
+
+INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-11e6-80f5-76304dec7eb7', 'd9f9206d-e54e-43c4-89ff-bc67c8a2581f');
