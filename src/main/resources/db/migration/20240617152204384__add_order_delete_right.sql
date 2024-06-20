721f0e9cf9ef428754c30e959758665e61b23765
Szymon Radziszewski <117299684+sradziszewski@users.noreply.github.com>
2024-06-20 09:35:52 +0200

    OAM-220: Added creating new geo zone for new facility (#107)
    * OAM-220: Added creating new geo zone for new facility
    
    * OAM-220: Fixed right ID in migration

--- src/main/resources/db/migration/20240617152204384__add_order_delete_right.sql
+++ src/main/resources/db/migration/20240617152204384__add_order_delete_right.sql
@@ -1,3 +1,3 @@
-INSERT INTO rights (id, description, name, type) VALUES ('60580166-ab1c-464a-8401-08384efc57b7', NULL, 'ORDERS_DELETE', 'ORDER_FULFILLMENT');
+INSERT INTO rights (id, description, name, type) VALUES ('a747c61f-d568-403e-98d6-cdd8df666c07', NULL, 'ORDERS_DELETE', 'ORDER_FULFILLMENT');
 
-INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-11e6-80f5-76304dec7eb7', '60580166-ab1c-464a-8401-08384efc57b7');
+INSERT INTO referencedata.role_rights (roleid, rightid) VALUES ('a439c5de-b8aa-11e6-80f5-76304dec7eb7', 'a747c61f-d568-403e-98d6-cdd8df666c07');
