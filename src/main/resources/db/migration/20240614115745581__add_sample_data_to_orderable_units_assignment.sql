d302dfbbbf64ac7650255c7477d962b69489c038
tsznaj <tsznajderski@soldevelo.com>
2024-06-20 12:17:57 +0200

    OAM-213: Fixed assigments.

--- src/main/resources/db/migration/20240614115745581__add_sample_data_to_orderable_units_assignment.sql
+++ src/main/resources/db/migration/20240614115745581__add_sample_data_to_orderable_units_assignment.sql
@@ -2,6 +2,6 @@ INSERT INTO orderable_units_assignment (orderableid, orderableversionnumber, uni
     SELECT o.id, o.versionnumber, 'c86e7d33-f8f8-4e0d-b540-89b16ffd71f2' FROM orderables o
 WHERE
     NOT EXISTS (
-        SELECT id FROM orderable_units_assignment WHERE id = o.id
-        AND versionnumber = o.versionnumber
+        SELECT * FROM orderable_units_assignment WHERE orderableid = o.id
+        AND orderableversionnumber = o.versionnumber
     );
