370597d487ea2939e482175ff8519272c9f3a3a6
Szymon Radziszewski <117299684+sradziszewski@users.noreply.github.com>
2024-07-08 11:02:52 +0200

    OIS-14: Upgrade Transifex API (#114)
    * OIS-14: Changed --resource value
    
    * OIS-14: Updated credentialsId in Jenkinsfile
    
    * OIS-14: Updated docker-dev version to v9
    
    * Reverted changes to Jenkinsfile

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Sync with Transifex
 /transifex/sync_transifex.sh \
-  --resource openlmis-referencedata.messages \
+  --resource openlmis-referencedata \
   --pattern 'src/main/resources/messages_<lang>.properties' \
   --source-file src/main/resources/messages_en.properties
 
