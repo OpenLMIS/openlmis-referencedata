58340a855e250c5ae84b579c10b9b62f33b4e4aa
Piotr Wargulak <pwargulak@soldevelo.com>
2024-11-05 16:19:57 +0100

    Added SIGECA Sync fallback FacilityType and GeoZone

---
+++ src/main/resources/db/migration/20241105160000000__add_sigeca_import_fallback.sql
@@ -0,0 +1,2 @@
+INSERT INTO referencedata.facility_types(id, active, code, description, displayorder, name, primaryhealthcare)
+VALUES ('4426e01d-7ed4-4bd3-82c2-878ce6a7705b', true, 'UNKNOWN', 'Facility imported from SIGECE Central with category that does not correspond to any Facility Type.', 0, 'UNKNOWN', false);
