dc3f3265f48c586efb06d6fefab081f9a9585860
michaldev0 <125133223+michaldev0@users.noreply.github.com>
2026-06-26 11:32:34 +0200

    OLMIS-8238: Regenerate user preferences migration

--- src/main/resources/db/migration/20260626092751954__create_user_preferences_table.sql
+++ src/main/resources/db/migration/20260626092751954__create_user_preferences_table.sql
@@ -1,7 +1,3 @@
--- WHEN COMMITTING OR REVIEWING THIS FILE: Make sure that the timestamp in the file name (that serves as a version) is the latest timestamp, and that no new migration have been added in the meanwhile.
--- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way.
--- Migrations should NOT BE EDITED. Add a new migration to apply changes.
-
 CREATE TABLE user_preferences (
     id uuid NOT NULL,
     userid uuid NOT NULL,
